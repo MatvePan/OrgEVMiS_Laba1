76273f61a05dc1e6689123a3f9ec2bfd712a5c25
MatvePan <159632905+MatvePan@users.noreply.github.com>
2024-10-27 20:55:03 +0300

    Add files via upload

--- OrgEVM1.cpp
+++ OrgEVM1.cpp
@@ -1,4 +1,7 @@
 ﻿#include "iostream"
+#include "limits"
+#include <cmath>
+#include <limits>
 using namespace std;
 // Вариант 15: unsigned int и long double
 
@@ -13,23 +16,52 @@ void UniIntBinaryTranslate(unsigned int number) {
 }
 
 void LonDueBinaryTranslate(long double number) {
-	int size = sizeof(long double);
 	unsigned char* str = reinterpret_cast<unsigned char*>(&number);
+	int size = sizeof(long double);
 	for (int i = size - 1; i >= 0; i--) {
 		for (int j = 7; j >= 0; j--) {
 			cout << ((str[i] >> j) & 1);
 		}
-		if (i == size - 2)
-			cout << ".";
+		if (i == size - 2) cout << ".";
+		cout << "|";
 	}
-	cout << "\n";
+	cout << endl;
+}
+
+unsigned int ShiftUniInt(unsigned int& uniint, int shift, int little, bool arrow, int bits) {
+	unsigned int mask = ((1u << bits) - 1) << little;
+	unsigned int cage = (uniint & mask) >> little;
+	if (arrow) {
+		cage = ((cage << shift) | (cage >> (bits - shift))) & ((1u << bits) - 1);
+	}
+	else {
+		cage = ((cage >> shift) | (cage << (bits - shift))) & ((1u << bits) - 1);
+	}
+	uniint &= ~mask;
+	uniint |= (cage << little);
+	return uniint;
+}
+
+long double ShiftLonDue(long double& londue, int shift, int little, bool arrow, int bits) {
+	uint64_t* p = reinterpret_cast<uint64_t*>(&londue);
+	uint64_t mask = (1ull << bits) - 1;
+	uint64_t bit = (*p >> little) & mask;
+	if (arrow) {
+		bit = (bit << shift) | (bit >> (little - shift));
+	}
+	else {
+		bit = (bit >> shift) | (bit << (little - shift));
+	}
+	*p = (*p & ~(mask << little)) | (bit << little);
+	return londue;
 }
 
 int main() {
 	setlocale(LC_ALL, "RU");
-	unsigned int uniint;
-	long double londue;
-	int unibit, lonbit;
+	unsigned int uniint, newint;
+	long double londue, newdouble;
+	int shift, little, bits;
+	int arrow;
 	cout << "Введите число типа unsigned int\n";
 	cin >> uniint;
 	cout << "Введите чисор типа long double\n";
@@ -42,5 +74,24 @@ int main() {
 	LonDueBinaryTranslate(londue);
 	cout << "\n";
 	cout << "Преобразования\n";
-
+	// Выполнить циклический сдвиг в заданную пользователем сторону на некоторое количество разрядов в пределах определённой группы разрядов, количество которых и номер младшего разряда в группе задаются с клавиатуры
+	cout << "Введите количество разрядов для сдвига:\n";
+	cin >> shift;
+	cout << "Введите младший разряд:\n";
+	cin >> little;
+	cout << "Введите количество битов (0-15):\n";
+	cin >> bits;
+	cout << "Введите сторону сдвига:\n";
+	cout << "1 - влево, 2 - вправо\n";
+	cin >> arrow;
+	if (arrow != 1 or arrow != 2) {
+		return 1;
+	}
+	bool boshiftleft = (arrow == 1);
+	newint = ShiftUniInt(uniint, shift, little, boshiftleft, bits);
+	newdouble = ShiftLonDue(londue, shift, little, boshiftleft, bits);
+	cout << "Преобразованное число типа unsigned int\n";
+	cout << newint << "\n";
+	cout << "Преобразованное число типа long double\n";
+	cout << newdouble << "\n";
 }
